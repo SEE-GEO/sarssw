22bae2c600ced8cb760bb65c1becca218a4bbaf9
Filip Borg <filipborg97@gmail.com>
2023-04-14 11:23:18 +0200

    Cleanup and add comments for download_sar_multiprocess.py

--- pipeline/download_sar_multiprocess.py
+++ pipeline/download_sar_multiprocess.py
@@ -3,7 +3,7 @@
 """
 This program downloads the sar images found in the bouy survey data result dataframe found in the folder specified by the variable bouy_survey_dir.
 It then extracts subimages for the bouy(s) contained within that image and save them to the folder specified by out_dir.
-This program assumes that you have your cridentials to datapool.asf.alaska.edu in the ~/.netrc file
+This program assumes that you have your credentials to datapool.asf.alaska.edu in the ~/.netrc file
 """
 
 import asf_search as asf
@@ -22,7 +22,7 @@ import multiprocessing
 import xarray as xr
 import xsar
 
-out_dir = '/data/exjobb/sarssw/sar_multiprocess3/'
+out_dir = '/data/exjobb/sarssw/sar_dataset/'
 tmp_dir = '/data/exjobb/sarssw/tmp/'
 bouy_survey_path = '/home/sarssw/filip/sarssw/bouy_survey/1h_survey/result_df'
 existing_img_dir = '/data/exjobb/sarssw/sar'
@@ -40,14 +40,14 @@ subimage_prop_offsets = {
     6:(-.5, -.5), 5:(-.5, 0), 4:(-.5, .5)
 }
 
-#Currently using .netrc file instead, to change to manual input uncomment these and adjust sar_download accordingly specifying the session argument in asf.download_url
+#Currently using .netrc file, to change to manual input uncomment these and adjust sar_download accordingly specifying the session argument in asf.download_url
 #username = input('Username:')
 #password = getpass.getpass('Password:')
 #session = asf.ASFSession().auth_with_creds(username=username, password=password)
 
 @contextlib.contextmanager
 def sar_download(url):
-    "Downloads the SAR images in a temporary directory"
+    "Downloads the SAR image from url in a temporary directory"
     #print('Downloading:', url)
     with tempfile.TemporaryDirectory(dir = tmp_dir) as tmp_dir_local:
         asf.download_url(url=url, path=tmp_dir_local)
@@ -61,8 +61,10 @@ def sar_download(url):
 
 @contextlib.contextmanager
 def sar_download_check_local(url):
-    """Downloads the SAR images in a temporary directory, 
-    Checks if the file exists localy in existing_img_dir and in that case uses that file"""
+    """
+    Downloads the SAR image from url in a temporary directory, 
+    also checks if the unzipped file exists locally in existing_img_dir and in that case uses that file
+    """
     #if files is found locally
     safe_name = url.split('/')[-1].split('.')[0] + '.SAFE'
     if os.path.isdir(existing_img_dir) and safe_name in os.listdir(existing_img_dir):
@@ -84,7 +86,19 @@ def sar_download_check_local(url):
             yield os.path.join(tmp_dir_local, zip_name.split('.')[0] + '.SAFE')
         
 def create_subimages(sar_name, bouy, bouy_df, sar_ds, dist):
-    "Extracts the subimages from one downloaded SAR image"
+    """
+    Extracts the subimages from one downloaded SAR image
+    
+    Inputs:
+    sar_name name of the SAR image
+    bouy: filename of the bouy
+    bouy_df: the part of the survey dataframe that is related to this sar_name bouy combination
+    sar_ds: the SAR image as a xsar.SarDataset object
+    dist: pixel distance from subimage center to edge in line and sample direction
+    
+    Side effects:
+    Saves the subimages from sar_ds for bouy to the folder specified by out_dir
+    """
     #print('Handling bouy:', bouy, 'in sar image', sar_name)
 
     bouy_name = bouy.split('.')[0]
@@ -93,9 +107,9 @@ def create_subimages(sar_name, bouy, bouy_df, sar_ds, dist):
     sar_xr = sar_ds.dataset
 
     #Return when the bouy is not found within the image,
-    #Can happen for images at the antimeridian
+    #Can happen for images at the antimeridian etc,
     if any(np.isnan([bouy_line, bouy_sample])):
-        print(f"Skipping {bouy} since it is not found inside the image {sar_name}")
+        #print(f"Skipping {bouy} since it is not found inside the image {sar_name}")
         return
     
     #Subimage limits as subimage_index:(line_min_offset, line_offset_center, line_max_offset, sample_min_offset, sample_offset_center, sample_max_offset)
@@ -184,7 +198,13 @@ def create_subimages(sar_name, bouy, bouy_df, sar_ds, dist):
 
 
 def handle_url(args):
-    "The main logic for handling one url"
+    """
+    The main logic for handling one url
+    
+    There are two parts:
+    First downloading the sar image
+    Then extracting the subimages
+    """
     url, bouy_survey_df = args
     #print('Handling url:', url)
 
@@ -194,7 +214,7 @@ def handle_url(args):
             sar_ds = xsar.Sentinel1Dataset(sar_meta)
 
             sar_name = url.split('/')[-1].split('.')[0]
-            dist = {
+            dist = { #Distance in pixels from center to edge of subimage
                 'line': int(np.round(box_size / 2 / sar_meta.pixel_line_m)),
                 'sample': int(np.round(box_size / 2 / sar_meta.pixel_sample_m))
             }
@@ -210,7 +230,12 @@ def handle_url(args):
         sys.exit(1)
 
 def handle_url_with_retry(args):
-    "Wraps exception handling around the handle_url"
+    """
+    Wraps exception handling and automatic retrying around the handle_url
+
+    Inputs:    
+    args is an list of url and bouy_survey_df
+    """
     url, bouy_survey_df = args
 
     for tries in range(1, handle_url_max_retries + 1):
@@ -228,27 +253,44 @@ def handle_url_with_retry(args):
                 print(f'skiping {url}')
                 return
     
-def multiprocess_urls(urls, bouy_survey_df):
-    "Multi processed handling of the urls"
+def multiprocess_urls(urls, bouy_survey_df, all_urls_len, processed_urls_len):
+    """
+    Multi processed handling of the urls
+    
+    Inputs:
+    urls: list of urlts to process
+    bouy_survey_df: pandas dataframe with bouy survey data
+    all_urls_len: length of all urls, it is the sum of len(urls) and processed_urls_len
+    processed_urls_len: number of urls that have already been processed
+    """
+    
     if num_processes == 1:
         for url in tqdm(urls):
             handle_url_with_retry((url, bouy_survey_df))
 
     else:
-        with multiprocessing.Pool(num_processes) as pool, multiprocessing.Manager() as manager:
+        with multiprocessing.Pool(num_processes) as pool:
             try:
-                shutdown = manager.Event()
-                for _ in tqdm(pool.imap_unordered(handle_url_with_retry, zip(urls, repeat(bouy_survey_df, len(urls)))), total=len(urls)):
+                for _ in tqdm(pool.imap_unordered(handle_url_with_retry, zip(urls, repeat(bouy_survey_df, len(urls)))), initial=processed_urls_len, total=all_urls_len):
                     pass
 
             except KeyboardInterrupt:
-                shutdown.set()
                 print('Parent keyboard interrupt caught, shutdown (waiting)')
                 pool.terminate()
                 print('Shutdown done')
                 sys.exit('Keyboard interrupt')
 
 def main():
+    """
+    The main program for loading the correct data and running the subimage extraction
+    """
+    
+    print(f'Downloading from bouy data found at {bouy_survey_path}')
+    print(f'Saving at subimages at {out_dir}')
+    print(f'Using tmpdir {tmp_dir}')
+    if tmp_dir is not None:
+        print(f'Predownloaded images is found at {existing_img_dir}')
+
     #Read survey data
     with open(bouy_survey_path,'rb') as f_r:
         bouy_survey_df = pickle.load(f_r)
@@ -264,11 +306,13 @@ def main():
     filenames = filter(lambda fn: fn.endswith('.nc'), os.listdir(out_dir))
     processed_urls = {fn.split('-')[0] for fn in filenames}
     urls = [url for url in all_urls if url.split('/')[-1].split('.')[0] not in processed_urls]
-    print(f'urls found {len(all_urls)}')
-    print(f'urls already processed {len(processed_urls)}')
+    all_urls_len = len(all_urls)
+    processed_urls_len = len(processed_urls)
+    print(f'urls found {all_urls_len}')
+    print(f'urls already processed {processed_urls_len}')
     print(f'urls to handle {len(urls)}')
     
-    multiprocess_urls(urls, bouy_survey_df)
+    multiprocess_urls(urls, bouy_survey_df, all_urls_len, processed_urls_len)
 
 if __name__ == "__main__":
     main()
