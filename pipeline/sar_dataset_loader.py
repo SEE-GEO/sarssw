0df3795400f29102a554eb645108f6e06c98234d
Filip Borg <filipborg97@gmail.com>
2023-04-14 16:24:34 +0200

    Add height adjustment for wind speed and convert read netcdf files in sar_dataset_loader

--- pipeline/sar_dataset_loader.py
+++ pipeline/sar_dataset_loader.py
@@ -49,7 +49,18 @@ class Var_results:
             self.lat,
             self.time,
         ]
+
+#to adjust the wind speed acording to the wind profile power law
+#found at https://en.wikipedia.org/wiki/Wind_profile_power_law
+#using exponent parameter of 0.11
+#Depth is the negative height
+def wspd_power_law(wspd, depth):
+    #If the height is 0 assume 10 meters
+    if depth == 0:
+        return wspd_power_law(wspd, 10)
     
+    return wspd * (10/(-1*depth))**0.11
+        
 def load_features_df(sar_paths, svc_file):
     "Extracts the features in a dictionar"
     with open(svc_file, 'rb') as f: svc = pickle.load(f)
@@ -58,57 +69,67 @@ def load_features_df(sar_paths, svc_file):
     
     print('Calculating features')
     for file_path in tqdm(sar_paths):
-        if not file_path.endswith('.tif'): continue
+        if not file_path.endswith('.nc'): continue
         
-        with tif.TiffFile(file_path) as tif_img:
+        xds =  xr.open_dataset(file_path)
+        #metadata_dict['file_name'].append(file_name)
+        #for metadata_key, metadata_value in tif_img.shaped_metadata[0].items():
+        #    metadata_dict[metadata_key].append(metadata_value)
+        
+        for pol, v in zip(xds.sigma0.coords['pol'].values, xds.sigma0.values):
+            if np.isnan(v).any(): continue
+
+            ubyte = img_as_ubyte((v - v.min()) / (v.max() - v.min()))
+            glcm = graycomatrix(ubyte, distances=[10], angles=[0], levels=256,
+                                symmetric=True, normed=True)
+
+            all_glcm_types = ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM']
+            sar_glcm = {gt:graycoprops(glcm, gt)[0, 0] for gt in all_glcm_types}
+
+            glcm_types =  ['homogeneity', 'dissimilarity', 'correlation']
+            hom_test = svc.predict([[sar_glcm[gt] for gt in glcm_types]])[0] == 0
             
-            #metadata_dict['file_name'].append(file_name)
-            #for metadata_key, metadata_value in tif_img.shaped_metadata[0].items():
-            #    metadata_dict[metadata_key].append(metadata_value)
+            #filename and polarization for given image
+            feature_dict['file_name'].append(file_path.split('/')[-1])
+            feature_dict['pol'].append(pol)
             
-            for v, pol in zip(tif_img.asarray(), tif_img.shaped_metadata[0]['pol']):
-                if np.isnan(v).any(): continue
-
-                ubyte = img_as_ubyte((v - v.min()) / (v.max() - v.min()))
-                glcm = graycomatrix(ubyte, distances=[10], angles=[0], levels=256,
-                                    symmetric=True, normed=True)
-
-                all_glcm_types = ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM']
-                sar_glcm = {gt:graycoprops(glcm, gt)[0, 0] for gt in all_glcm_types}
-
-                glcm_types =  ['homogeneity', 'dissimilarity', 'correlation']
-                hom_test = svc.predict([[sar_glcm[gt] for gt in glcm_types]])[0] == 0
-                
-                #filename and polarization for given image
-                feature_dict['file_name'].append(file_path.split('/')[-1])
-                feature_dict['pol'].append(pol)
-                
-                #metadata from image
-                for metadata_key, metadata_value in tif_img.shaped_metadata[0].items():
-                    if metadata_key not in ['pol']:
-                        feature_dict[metadata_key].append(metadata_value)
-                
-                #bool for if image is homogenous or not
-                feature_dict['hom_test'].append(hom_test)
-                
-                #all features extracted from glcm
-                for glcm_type, glcm_value in sar_glcm.items():
-                    feature_dict[glcm_type].append(glcm_value)
-                
-                #features from sigma0 aggregations
-                feature_dict['sigma_mean'].append(v.mean())
-                feature_dict['sigma_var'].append(v.var())
-                feature_dict['sigma_mean_over_var'].append(v.mean() / v.var())
-                feature_dict['sigma_min'].append(v.min())
-                feature_dict['sigma_max'].append(v.max())
-                feature_dict['sigma_range'].append(v.max() - v.min())
+            #metadata from image
+            for metadata_key, metadata_value in xds.attrs.items():
+                if metadata_key not in ['pol']:
+                    feature_dict[metadata_key].append(metadata_value)
+            
+            #bool for if image is homogenous or not
+            feature_dict['hom_test'].append(hom_test)
+            
+            #all features extracted from glcm
+            for glcm_type, glcm_value in sar_glcm.items():
+                feature_dict[glcm_type].append(glcm_value)
+            
+            #features from sigma0 aggregations
+            feature_dict['sigma_mean'].append(v.mean())
+            feature_dict['sigma_var'].append(v.var())
+            feature_dict['sigma_mean_over_var'].append(v.mean() / v.var())
+            feature_dict['sigma_min'].append(v.min())
+            feature_dict['sigma_max'].append(v.max())
+            feature_dict['sigma_range'].append(v.max() - v.min())
 
     return pd.DataFrame(feature_dict)
 
-def load_labels_df(bouy_survey_fn, swh_model_fn, wspd_model_fn, sar_bouy_df):
-    "This function loads the labels for files indexed with sar_name and bouy_name, those pairs are given in teh sar_bouy_df argument"
+def load_labels_df(bouy_survey_path, swh_model_path, wspd_model_path, sar_bouy_df):
+    """
+    This function loads the labels for files indexed with sar_name and bouy_name, those pairs are given as columns in sar_bouy_df
+    
+    Parameters
+    bouy_survey_path: path to the survey dataframe
+    swh_model_path: path to the wave height model
+    wspd_model_path: path to the wind speed model
+    
+    Returns
+    A dataframe with the labels
+    """
 
-    with open(bouy_survey_fn,'rb') as f_r:
+    #Load the bouy survey dataframe
+    with open(bouy_survey_path,'rb') as f_r:
         bouy_survey_df = pickle.load(f_r)
         
     #Reindex to enable faster lookup
@@ -118,13 +139,21 @@ def load_labels_df(bouy_survey_fn, swh_model_fn, wspd_model_fn, sar_bouy_df):
 
     #wave height model
     print("Loading wave height model")
-    SWH_model = xr.open_dataset(swh_model_fn)
+    SWH_model = xr.open_dataset(swh_model_path)
 
     #Wind speed model
-    print("Loading wave speed model")
-    WSPD_model = xr.open_dataset(wspd_model_fn)
+    print("Loading wind speed model")
+    WSPD_model = xr.open_dataset(wspd_model_path)
 
     #Configure and program how the models work
+    
+    #Program how the labels are loaded from the survey dataframe
+    #His saves wave high as is and hight adjusts wind speed to 10m using the power law
+    suvey_value_functions = {
+        'SWH': lambda row: row['bouy_variable_value'],
+        'WSPD': lambda row: wspd_power_law(row['bouy_variable_value'], row['bouy_depth']),
+    }
+    
     var_list = ['SWH', 'WSPD']
     var_names = {
         'SWH': ['VHM0', 'VAVH'],
@@ -162,7 +191,7 @@ def load_labels_df(bouy_survey_fn, swh_model_fn, wspd_model_fn, sar_bouy_df):
                 'WSPD_time':np.dtype('<M8[ns]'), #np.datetime64
             }.items()})
     
-    #Filer the sar_bouy_df (index) dataframe to only include the relevant columns
+    #Filter the sar_bouy_df (index) dataframe to only include the relevant columns
     sar_bouy_df = sar_bouy_df[['sar_name', 'bouy_name']]
 
     print('Collecting labels')
@@ -173,6 +202,7 @@ def load_labels_df(bouy_survey_fn, swh_model_fn, wspd_model_fn, sar_bouy_df):
         }
 
         #Extract value(s) form survey
+        #The result can contain multiple rows, one for each measurement (wind speed, wave height as VHM0 and/or VAVH)
         survey_results = bouy_survey_df.loc[(sar_name, bouy_name)]
         #save longitude, latitude and time for eventual model search
         lon, lat, start_time, end_time = survey_results.iloc[0][['bouy_longitude', 'bouy_latitude', 'sar_start_time', 'sar_stop_time']]
@@ -184,7 +214,7 @@ def load_labels_df(bouy_survey_fn, swh_model_fn, wspd_model_fn, sar_bouy_df):
                 if (result['bouy_variable_name'] in var_names[var]) and (not result_vars[var].found):
                     result_vars[var].found = True
                     result_vars[var].source = 'bouy'
-                    result_vars[var].value = result['bouy_variable_value']
+                    result_vars[var].value = suvey_value_functions[var](result)
                     result_vars[var].lon = result['bouy_longitude']
                     result_vars[var].lat = result['bouy_latitude']
                     result_vars[var].time = result['bouy_time']
