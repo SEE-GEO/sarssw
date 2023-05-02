8e5663715fcc31a9f22f050126bc5a360167ee47
Filip Borg <filipborg97@gmail.com>
2023-05-02 22:31:43 +0200

    Add filtering parameters to data_copy

--- pipeline/data_copy.py
+++ pipeline/data_copy.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """
 This script is used to move the files for training, testing and/or validation to the tempdir on Alvis
+
+For example running:
+python data_copy.py --src /mimer/NOBACKUP/priv/chair/sarssw/sar_dataset/ --dest ${TMPDIR}/data --data_df /mimer/NOBACKUP/priv/chair/sarssw/sar_dataset_features_labels_27_april/sar_dataset_split.pickle --train --train_limit 1000 --val --val_limit 33 --non_nan --homogeneous --swath 'IW' --polarisation 'VV VH'
+In slurm would save 1000 train and 33 validation images in ${TMPDIR}/data filtered for non_nan values, homogeneouity, swath=IW and polarisation=VV VH
 """
 
 import pickle
@@ -17,16 +21,22 @@ if __name__ == "__main__":
     parser.add_argument('--dest', required=True, help='The destination path on alvis, most likely the $TMPDIR variable in the slurm script')
     parser.add_argument('--src', required=True, help='The path to the source folder of the images described in --data_df')
     parser.add_argument('--data_df', required=True, help='The path to the dataframe containg the metadata, features and labels for the images')
-    parser.add_argument('--train', action='store_true', help='Flag to enable copying of the train data')
-    parser.add_argument('--test', action='store_true', help='Flag to enable copying of the test data')
-    parser.add_argument('--val', action='store_true', help='Flag to enable copying of the validation data')
-    parser.add_argument('--limit', type=int, default=None, help='Set a limit on the number of files to save, this limit is applies separately to the datasets')
+    parser.add_argument('--train', action='store_true', help='Flag to enable copying of the train dataset')
+    parser.add_argument('--test', action='store_true', help='Flag to enable copying of the test dataset')
+    parser.add_argument('--val', action='store_true', help='Flag to enable copying of the validation dataset')
+    parser.add_argument('--train_limit', type=int, default=None, help='Set a limit on the number of files saved for the train dataset. Not specifying gives all data')
+    parser.add_argument('--test_limit', type=int, default=None, help='Set a limit on the number of files saved for the test dataset. Not specifying gives all data')
+    parser.add_argument('--val_limit', type=int, default=None, help='Set a limit on the number of files saved for the val dataset. Not specifying gives all data')
+    parser.add_argument('--non_nan', action='store_true', help='Enables filtering for only data without nans')
+    parser.add_argument('--homogeneous', action='store_true', help='Enables filtering for only data that passed the homogeneous filter')
+    parser.add_argument('--swath', help="Enables filtering to only include the specified swaths, seperate the swatchs with comma. i.e 'IW,EW' to include both IW and EW")
+    parser.add_argument('--polarisations', help="Enables filtering to only include images with the specified polarisations, seperate the polarisations with comma. i.e 'VV VH,HH HV' would include files with both VV VH and HH HV polarisations")
     parser.add_argument('--debug', action='store_true', help='Used for debugging. Print the number of files that would be copied if this flag was not present')
 
     args = parser.parse_args()
-    datasets = [(args.train, 'train'), (args.test, 'test'), (args.val, 'val')] 
+    datasets = [(args.train, 'train', args.train_limit), (args.test, 'test', args.test_limit), (args.val, 'val', args.val_limit)] 
 
-    if not any([flag for (flag, _) in datasets]):
+    if not any([flag for (flag, _, _) in datasets]):
         print("No dataset specified to be copied")
         sys.exit(1)
 
@@ -35,11 +45,31 @@ if __name__ == "__main__":
         fl_df = pickle.load(f)
 
     #filter data
-    #Here we can add filtering to only keep certain types of data
+    if args.non_nan:
+        fl_df = fl_df[~fl_df.isna().apply(any, axis=1)]
+
+    if args.homogeneous:
+        fl_df = fl_df[fl_df['hom_test']]
+
+    if args.swath is not None:
+        print(f"args.swath: {args.swath}")
+        print(f"type(args.swath): {type(args.swath)}")
+        swath_filter = [s.strip() for s in args.swath.split(',')]
+        print(f"swath_filter: {swath_filter}")
+        fl_df = fl_df[fl_df['swath'].isin(swath_filter)]
+
+
+    if args.polarisations is not None:
+        print(f"args.polarisations: {args.polarisations}")
+        print(f"type(args.polarisations): {type(args.polarisations)}")
+        pol_filter = [p.strip() for p in args.polarisations.split(',')]
+        print(f"pol_filter: {pol_filter}")
+        fl_df = fl_df[fl_df['polarisations'].isin(pol_filter)]
+
     #fl_df = fl_df[fl_df['swath'] == 'IW']
     #fl_df = fl_df[fl_df['pol'].isin(['HH', 'HV'])]
 
-    dataset_filter = [name for (flag, name) in datasets if flag]
+    dataset_filter = [(name, limit) for (flag, name, limit) in datasets if flag]
 
     all_imgs = set(os.listdir(args.src))
 
@@ -47,13 +77,13 @@ if __name__ == "__main__":
     if args.debug:
         print(f"Without the --debug flag this command would result in:")
 
-    for dataset_name in dataset_filter:
+    for dataset_name, dataset_limit in dataset_filter:
         copy_dest = os.path.join(args.dest, dataset_name)
         #Create dictionary
         os.makedirs(copy_dest, exist_ok=True)
 
         fl_df_dataset = fl_df[fl_df['split'] == dataset_name]
-        copy_imgs = all_imgs.intersection(set(fl_df_dataset['file_name'].unique()[:args.limit]))
+        copy_imgs = all_imgs.intersection(set(fl_df_dataset['file_name'].unique()[:dataset_limit]))
 
         #only print debug information about the copy
         if args.debug:
